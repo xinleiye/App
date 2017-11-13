304b979d8eb52273464d89734dbbc546de52e040
xinleiye <xinlei_ye@163.com>
2017-11-13 23:39:35 +0800

    vuex模块化尝试
    1. 尝试着将各个功能的状态模块化

--- src/store/store.js
+++ src/store/store.js
@@ -37,3 +37,74 @@ export default new Vuex.Store({
     mutations,
     actions
 });
+
+// 登陆模块
+const loginModule = {
+    state: {
+        userName: "",
+        userId: "",
+        status: 0
+    },
+    mutations: {
+        doLogin(state, data) {
+            state.userName = data.username;
+            state.userId = data.id;
+            state.status = data.id;
+        }
+    },
+    actions: {
+        doLogin(content, data) {
+            content.commit("doLogin", data);
+        }
+    },
+    getters: {
+        getStatus: (state) => {
+            return state.status;
+        }
+    }
+}
+
+// 学生模块
+const studentModule = {
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    }
+}
+
+// 教师模块
+const teacherModule = {
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    }
+}
+
+// 教委模块
+const managerModule = {
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    }
+}
+
+const store = new Vuex.Store({
+    modules: {
+        login: loginModule,
+        student: studentModule,
+        teacher: teacherModule,
+        manager: managerModule
+    }
+});
